4976da2c58316e3c74e7510d4c91ca6df9c48d7f
Howard Ding <hding2@illinois.edu>
2019-02-11 09:45:05 -0600

    ensure pid directory exists

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=`dirname $0`
 unset FITS_HOME
 
+mkdir -p tmp/pids
+
 #start ruby server
 bundle install
 bundle exec passenger start --port 4567 --environment production --log-file log/ruby-fits-server.log --pid-file tmp/pids/ruby-fits-server.pid &
